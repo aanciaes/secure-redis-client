d3e57aabbc17edb6e7873f15d9a84417987815e5
aanciaes <aanciaes.miguel@gmail.com>
2020-11-10 16:22:16 +0000

    Closes #3 Add multiple conatiner support for docker stats script

--- docker-stats-recorder.sh
+++ docker-stats-recorder.sh
@@ -3,21 +3,29 @@
 package='Record Docker Stats'
 
 # Variables
-container_id=;
+container_ids=;
+container_ids_array=;
 output_file=;
 interval=5
 samples=1
 command='docker stats --no-stream --format "{{.ID}},{{.Name}},{{.CPUPerc}},{{.MemPerc}},{{.MemUsage}},{{.NetIO}},{{.BlockIO}},{{.PIDs}}"'
 
+function join_by { local d=$1; shift; local f=$1; shift; printf %s "$f" "${@/#/$d}"; }
+
 run_init_assertions() {
-	if [ -z "$container_id" ]; then
+	if [ -z "$container_ids" ]; then
 		print_usage ;
 	else
-		command+=" $container_id"
+		IFS=',' read -ra container_ids_array <<< $container_ids
+
+		for i in "${container_ids_array[@]}"; do
+    		command+=" $i"
+		done
 	fi 
 
 	if [ -z "$output_file" ]; then
-		output_file="./$container_id-stats.csv"
+		join_containers_name=$(join_by "_" ${container_ids_array[@]})
+		output_file="$join_containers_name-stats.csv"
 	fi
 
 	if [ ! -f "${output_file}" ] || [ ! -s "${output_file}" ]; then 
@@ -37,12 +45,12 @@ print_usage() {
   echo "usage: $ docker-stats-recorder -c <container_id> [options]"
   echo " "
   echo "options:"
-  echo "-h                      Show help (this screen)"
-  echo "-c <container_id>       Indicate container to record"
-  echo "-o <output_name>        Indicate where to store stats"
-  echo "-f <csv>     	        Indicate the format of stored stats"
-  echo "-i <interval seconds>   Interval between samples"
-  echo "-s <number of samples>  Number of samples to recover. Set -1 to infinite"
+  echo "-h                      			Show help (this screen)"
+  echo "-c <container_id>,<container_id>    Indicate container to record"
+  echo "-o <output_name>        			Indicate where to store stats"
+  echo "-f <csv>     	        			Indicate the format of stored stats"
+  echo "-i <interval seconds>   			Interval between samples"
+  echo "-s <number of samples>  			Number of samples to recover. Set -1 to infinite"
 
   exit 0
 }
@@ -50,7 +58,7 @@ print_usage() {
 while getopts c:o:i:s: option; do
 	case "${option}" in
 		h) print_usage ;;
-		c) container_id=${OPTARG};;
+		c) container_ids=${OPTARG};;
 		o) output_file=${OPTARG};;
 		i) interval=${OPTARG};;
 		s) samples=${OPTARG};;
@@ -65,11 +73,12 @@ while [ $index -lt $samples ] || [ $samples == -1 ]; do
 	echo "Recording a sample..."
 
 	timestamp=$(date +%s%N)
-	output=$(eval $command)
-	output+=",$timestamp"
-	
-	echo "$output" >> "$output_file"
 
+	eval $command | while IFS= read -r output_line ; do
+		output_line+=",$timestamp"
+		echo "$output_line" >> "$output_file"
+	done
+	
 	((index++))
 
 	if [ $index -lt $samples ] || [ $samples == -1 ]; then
